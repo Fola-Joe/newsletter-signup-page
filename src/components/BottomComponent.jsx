f2d7e6f204de0416ebc9be6c357eee74d5d5ff72
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2024-04-11 18:15:54 +0100

    mobile-view in progress

--- src/components/BottomComponent.jsx
+++ src/components/BottomComponent.jsx
@@ -1,18 +1,29 @@
+import './BottomComponent.css';
+
 export default function BottomComponent() {
     return (
         <div className="BottomComponent">
-            <h1>Stay Updated</h1>
-            <h2>Join 60,000+ product managers receiving monthly updates on:</h2>
-            <ul>
-                <li>Product discovery and building what matters</li>
-                <li>Measuring to ensure updates are a success</li>
-                <li>And much more!</li>
+            <h1 className="heading">Stay Updated!</h1>
+            <h2 className="line-1">Join 60,000+ product managers receiving monthly updates on:</h2>
+            <ul className="list">
+                <li>
+                    <img src="images/icon-list.svg" alt="" />
+                    <p>Product discovery and building what matters</p>
+                </li>
+                <li>
+                    <img src="images/icon-list.svg" alt="" />
+                    <p>Measuring to ensure updates are a success</p>
+                </li>
+                <li>
+                    <img src="images/icon-list.svg" alt="" />
+                    <p>And much more!</p>
+                </li>
             </ul>
 
-            <form action="">
-                <h3>Email address</h3>
+            <form action="" className="form">
+                <h3 className="email">Email address</h3>
                 <input type="email" name="email" id="email" aria-aria-labelledby="Email address" />
-                <button>Subscribe to monthly newsletter</button>
+                <button className="btn">Subscribe to monthly newsletter</button>
             </form>
         </div>
     )
